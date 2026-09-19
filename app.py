740e656613e0a99e984313226f28e8c5baf8ebad
kikidookidscollection-cmd <kikidoo.kidscollection@gmail.com>
2026-09-19 15:02:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def scrape_website_text(url):
 if generate_btn and api_key and user_input:
     try:
         genai.configure(api_key=api_key)
-        model = genai.GenerativeModel('gemini-1.5-flash-latest')
+        model = genai.GenerativeModel('gemini-pro')
         
         video_context = ""
         
